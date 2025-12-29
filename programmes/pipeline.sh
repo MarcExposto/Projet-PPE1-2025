c22e89d2e13be8251c89b9600100e952e31191d6
Lena Baraquin <baraquin.lena@gmail.com>
2025-12-29 21:55:36 +0100

    switch programmes_bis / programmes

--- programmes/pipeline.sh
+++ programmes/pipeline.sh
@@ -2,21 +2,21 @@
 
 # Vérification des arguments
 if [ -z "$1" ]; then
-  echo "Veuillez entrer le chemin vers le fichier d'URLs en premier argument."
+  echo "Veuillez entrer le chemin vers le fichier d'URLs en premier argument. (script ${0})"
   exit
 else
   FichierUrls=$1
 fi
 
 if ! [[ "$2" =~ ^(en)|(fa)|(fr)$ ]]; then
-  echo "Le deuxième argument doit être le code ISO-639-1 de la langue."
+  echo "Le deuxième argument doit être le code ISO-639-1 de la langue. (script ${0})"
   exit
 else
   Langue=$2
 fi
 
 if [ -z "$3" ]; then
-  echo "Veuillez entrer le mot cible en troisième argument."
+  echo "Veuillez entrer le mot cible en troisième argument. (script $0)"
   exit
 else
   MotCible=$3
@@ -29,7 +29,7 @@ CheminRacineProjet="$(cd "${CheminScript}/.." && pwd)"
 # Génération du tableau (tsv)
 id=1
 
-echo "id \t url \t gestion de robots.txt \t code http \t encodage initial de la page \t lien vers la page brute \t statut de la conversion en UTF-8 \t lien vers le dump textuel \t numbre total de mots dans le dump \t nombre d'occurrences du mot cible dans le dump \t lien vers le concordancier \t lien vers l'analyse des bigrammes"
+printf "id \t url \t gestion de robots.txt \t code http \t encodage initial de la page \t lien vers la page brute \t statut de la conversion en UTF-8 \t lien vers le dump textuel \t numbre total de mots dans le dump \t nombre d'occurrences du mot cible dans le dump \t lien vers le concordancier \t lien vers l'analyse des bigrammes\n"
 
 while read -r line; do
 
@@ -37,7 +37,7 @@ while read -r line; do
 
   Robots=$(${CheminRacineProjet}/programmes/scripts/robots.sh $Url)
   if [[ $Robots = 'Disallow' ]]; then
-    echo "${id}\t${Url}\t${Robots}\t\t\t\t\t\t\t\t\t"
+    printf "${id}\t${Url}\t${Robots}\t\t\t\t\t\t\t\t\t\n"
     ((id++))
     continue
   else
@@ -48,8 +48,8 @@ while read -r line; do
   data=$(curl -s -i -L -w "%{http_code}\n%{content_type}" -o ${CheminFichierAspiration} ${Url})
 
   CodeHTTP=$(echo "$data" | head -1)
-  if [[ ! $CodeHTTP =~ "(2|3).." ]]; then
-    echo "${id}\t${Url}\t${Robots}\t${CodeHTTP}\t\t\t\t\t\t\t\t"
+  if [[ ! $CodeHTTP =~ (2|3).. ]]; then
+    printf "${id}\t${Url}\t${Robots}\t${CodeHTTP}\t\t\t\t\t\t\t\t\n"
     ((id++))
     continue
   fi
@@ -61,7 +61,7 @@ while read -r line; do
 
   ErreurConversion=$(${CheminRacineProjet}/programmes/scripts/convert_utf-8.sh ${CheminFichierAspiration} ${Encodage} 2>&1)
   if [[ -n $ErreurConversion ]]; then
-    echo "${id}\t${Url}\t${Robots}\t${CodeHTTP}\t${Encodage}\tÉchec\t\t\t\t\t\t"
+    printf "${id}\t${Url}\t${Robots}\t${CodeHTTP}\t${Encodage}\tÉchec\t\t\t\t\t\t\n"
     ((id++))
     continue
   fi
@@ -70,7 +70,7 @@ while read -r line; do
   ${CheminRacineProjet}/programmes/scripts/text_dump.sh $CheminFichierAspiration $CheminFichierTexte
 
   NbMots=$(cat ${CheminFichierTexte} | wc -w)
-  NbOccurrences=$(egrep ${MotCible} ${CheminFichierTexte})
+  NbOccurrences=$(egrep -o ${MotCible} ${CheminFichierTexte} | wc -w)
 
   CheminFichierTokens="${CheminRacineProjet}/dumps-tokenises/${Langue}/${Langue}-${id}.conll"
   ${CheminRacineProjet}/programmes/scripts/tokenizer.sh $CheminFichierTexte $CheminFichierTokens
@@ -83,7 +83,7 @@ while read -r line; do
   echo XXX >$CheminFichierBigrammes
   #${CheminRacineProjet}/programmes/scripts/bigrammes.sh XXX
 
-  echo "${id}\t${Url}\t${Robots}\t${CodeHTTP}\t${Encodage}\tConverti\t[Lien vers le dump](${CheminFichierTexte})\t${NbMots}\t${NbOccurrences}\t[Lien vers le fichier tokenisé](${CheminFichierTokens})\t[Lien vers le concordancier](${CheminFichierConcordances})\t[Lien vers les bigrammes](${CheminFichierBigrammes})"
+  printf "${id}\t${Url}\t${Robots}\t${CodeHTTP}\t${Encodage}\tConverti\t[Lien vers le dump](${CheminFichierTexte})\t${NbMots}\t${NbOccurrences}\t[Lien vers le fichier tokenisé](${CheminFichierTokens})\t[Lien vers le concordancier](${CheminFichierConcordances})\t[Lien vers les bigrammes](${CheminFichierBigrammes})\n"
 
   ((id++))
 done <$FichierUrls
@@ -93,4 +93,4 @@ CheminFichierCoocurrents="../coocurents/${Langue}.tsv"
 ./scripts/coocurrents.sh ../dumps-tokenises/${Langue} $Monmot $CheminFichierCoocurrents
 
 # Génération du nuage de mots
-XXX
+#XXX
